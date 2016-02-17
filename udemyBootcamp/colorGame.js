8e035bb5fac5159a8608d3632f8c638f35f0b0e7
Sam <sam.ringleman@gmail.com>
2016-02-17 14:02:05 -0700

    more on the color picker

--- udemyBootcamp/colorGame.js
+++ udemyBootcamp/colorGame.js
@@ -1,26 +1,27 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var modeBtns = document.querySelectorAll(".mode");
+
+for(var i = 0; i < modeBtns.length; i++){
+  modeBtns[i].addEventListener("click", function(){
+    modeBtns[0].classList.remove("selected");
+    modeBtns[1].classList.remove("selected");
+    this.classList.add("selected");
+    this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+    reset();
+  })
+}
 
 colorDisplay.textContent = pickedColor;
 
 resetButton.addEventListener("click", function(){
-  //generate all new colors
-  colors = generateRandomColors(6);
-  //pick a new random color from arr
-  pickedColor = pickColor();
-  //change colorDisplay to match new picked color
-  colorDisplay.textContent = pickedColor; 
-  //change colors of squares on the page
-  for(var i=0; i<squares.length; i++){
-    squares[i].style.background = colors[i];
-  }
-  //change h1 background back to original color
-  h1.style.background = "#232323";
+  reset();
 })
 
 for (var i = 0; i < squares.length; i++) {
@@ -33,8 +34,10 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.background;
     //compare color to pickedColor
     if(clickedColor === pickedColor){
+      //This is what happens when you win
       messageDisplay.textContent = "Correct";
       changeColors(clickedColor);
+      resetButton.textContent = "Play Again?";
       h1.style.background = clickedColor;
     } else {
       this.style.background = "#232323";
@@ -77,4 +80,27 @@ function randomColor(){
   var b = Math.floor(Math.random() * 256)
   var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
   return rgb;
+}
+
+function reset(){
+   //generate all new colors
+  colors = generateRandomColors(numSquares);
+  //pick a new random color from arr
+  pickedColor = pickColor();
+  //change colorDisplay to match new picked color
+  colorDisplay.textContent = pickedColor; 
+  //change colors of squares on the page
+  for(var i=0; i<squares.length; i++){
+    if(colors[i]){
+      squares[i].style.display = "block";
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+  //change h1 background back to original color
+  h1.style.background = "steelblue";
+  //change text to say New Colors
+  resetButton.textContent = "New Colors";
+  messageDisplay.textContent = "";
 }
