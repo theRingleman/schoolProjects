b874aa706bbd87bcffa9d1870e604fe05fcceaee
Sam <sam.ringleman@gmail.com>
2016-03-06 18:47:44 -0700

    Added Head First directory

---
+++ headFirst/battleship.js
@@ -0,0 +1,30 @@
+var location1 = Math.floor(Math.random() * 5);
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while(!isSunk){
+  guess = prompt("Ready, aim, fire! (enter a number from 0 - 6)");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+  } else {
+    guesses++;
+    if(guess == location1 || guess == location2 || guess == location3){
+      hits++;
+      alert("Hit!");
+      if(hits == 3){
+        isSunk = true;
+        alert("You sunk my battleship!");
+      }
+    } else {
+      alert("Miss")
+    }
+  }
+}
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+            "which means your shooting accuracy was " + (3/guesses);
+alert(stats); 
---
+++ headFirst/unscramble.js
@@ -0,0 +1,9 @@
+var i = 0;
+var name = "Joe";
+
+while(i < 2) {
+  document.write("Happy Birthday to you. <br>");
+  i++
+}
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you");
