0c4572db31e85f48d75669d1958e9a2a37081a50
Sam <sam.ringleman@gmail.com>
2016-03-06 23:49:06 -0700

    Added the object section of Head First

--- headFirst/taxi.js
+++ headFirst/taxi.js
@@ -25,8 +25,36 @@ var fiat = {
   color: "Medium Blue",
   passengers: 2,
   convertible: false,
-  mileage: 88000
-}
+  mileage: 88000,
+  started: false,
+  fuel: 0,
+  start: function() {
+    if(this.fuel > 0) {
+      this.started = true;
+    } else {
+      alert("This car is on empty, fill up before starting!");
+    }
+  },
+  stop: function() {
+    this.started = false;
+  },
+  drive: function() {
+    if (this.started) {
+      if (this.fuel > 0) {
+        alert(this.make + " " + this.model + " goes zoom zoom!");
+        this.fuel--;
+      } else {
+        alert("Uh oh, out of fuel");
+        this.stop();
+      }
+    } else {
+      alert("You need to start the engine first.");
+    }
+  },
+  addFuel: function(amount){
+    this.fuel = this.fuel + amount;
+  }
+};
 
 var chevy = {
   make: "Chevy",
@@ -36,6 +64,20 @@ var chevy = {
   passengers: 2,
   convertible: "false",
   mileage: 1021
+}
+
+var eightBall = {
+  index: 0,
+  advice: ["yes", "no", "maybe", "not a chance"],
+  shake: function() {
+    this.index = this.index + 1;
+    if (this.index >= this.advice.length) {
+      this.index = 0;
+    }
+  },
+  look: function() {
+    return this.advice[this.index];
+  }
 }
 
 worthIt(taxi);
