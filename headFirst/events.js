95c2f61c55a5bf358b439b5b5d87bd7739594bd9
Sam <sam.ringleman@gmail.com>
2016-03-09 16:48:37 -0700

    Here are the files and code for the bootstrap wordpress course

--- headFirst/events.js
+++ headFirst/events.js
@@ -1,16 +1,41 @@
 window.onload = init;
 
 
+
 function init() {
   var images = document.querySelectorAll("img");
   for (var i = 0; i < images.length; i++) {
-    images[i].onclick = showAnswer;
+    images[i].onmouseover = showAnswer;
+    images[i].onmouseout = reblur;
   }
 }
 
 function showAnswer(eventObj) {
   var image = eventObj.target;
   var name = image.id;
-  name = name + ".jpg";
+  var newName = name + ".jpg";
+  image.src = newName;
+
+  // setTimeout(reblur, 2000, image);
+}
+
+function reblur(eventObj) {
+  var image = eventObj.target;
+  var name = image.id;
+  name = name + "blur.jpg";
   image.src = name;
 }
+
+// function init() {
+//   var images = document.querySelectorAll("img");
+//   for (var i = 0; i < images.length; i++) {
+//     images.onmouseover = showAnswer;
+//   }
+// }
+
+// function showAnswer(eventObj) {
+//   var image = eventObj.target;
+//   var name = image.id;
+//   name = name + ".jpg";
+//   image.src = name;
+// }
