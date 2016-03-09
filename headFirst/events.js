2990f587bdfeb10ef7df6bec823ae8dfc5c3e2bd
Sam Ringleman <Sam@Sams-Air.PK5001Z>
2016-03-09 11:12:57 -0700

    Add the events app for head first along with a slew of pictures

---
+++ headFirst/events.js
@@ -0,0 +1,16 @@
+window.onload = init;
+
+
+function init() {
+  var images = document.querySelectorAll("img");
+  for (var i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
+}
+
+function showAnswer(eventObj) {
+  var image = eventObj.target;
+  var name = image.id;
+  name = name + ".jpg";
+  image.src = name;
+}
