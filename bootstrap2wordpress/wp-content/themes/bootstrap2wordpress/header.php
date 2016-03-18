060b41d7b17b3cb14ef0f5c8bc4d5df32543d36a
Sam Ringleman <Sam@Sams-Air.PK5001Z>
2016-03-18 15:57:02 -0600

    hopefully fixed the wp css image issue and added in the page template so far.

--- bootstrap2wordpress/wp-content/themes/bootstrap2wordpress/header.php
+++ bootstrap2wordpress/wp-content/themes/bootstrap2wordpress/header.php
@@ -2,25 +2,24 @@
 /**
  * The header for our theme.
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
+ * Displays all of the <head> section and everything up till <div id="content">
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package bootstrap2wordpress
+ * @package Bootstrap to WordPress
  */
-
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <!-- Bootstrap core CSS -->
 <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- Font Awesome Icons -->
+<!-- FontAwesome Icons -->
 <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
 <!-- Google Fonts -->
@@ -30,21 +29,21 @@
 
 <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrap2wordpress' ); ?></a>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'bootstrap2wordpress' ); ?></a>
 
 	<!-- HEADER
 	================================================== -->
 	<header class="site-header" role="banner">
 
-		<!-- Navbar -->
+		<!-- NAVBAR
+		================================================== -->
 		<div class="navbar-wrapper">
 
 			<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -57,23 +56,28 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a href="#" class="navbar-brand"><img src="<?php bloginfo('stylesheet_directory') ?>/assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
-					</div> <!-- navbar header -->
+
+						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a>
+
+					</div><!-- navbar-header -->
+
+					<!-- If the menu (WP admin area) is not set, then the "menu_class" is applied to "container". In other words, it overwrites the "container_class". Ref: http://wordpress.org/support/topic/wp_nav_menu-menu_class-usage-bug?replies=4 -->
 
 					<?php
-						wp_nav_menu(array(
-							'theme_location'		=> 'primary',
-							'container'					=> 'nav',
-							'container_class'		=> 'navbar-collapse collapse',
-							'menu_class'				=> 'nav navbar-nav navbar-right'
-						));
-					 ?>
+						wp_nav_menu( array(
 
+							'theme_location'	=> 'primary',
+							'container'			=> 'nav',
+							'container_class'	=> 'navbar-collapse collapse',
+							'menu_class'		=> 'nav navbar-nav navbar-right'
 
-				</div> <!-- container -->
+						) );
+					?>
 
-			</div> <!-- navbar -->
+				</div><!-- container -->
 
-		</div> <!-- navbar wrapper -->
+			</div><!-- navbar -->
+
+		</div><!-- navbar-wrapper -->
 
 	</header>
